778ebcb75c99bd91d85d74276afef81b69f1b2c2
Romeo LOUATI <romeo.louati@ensiie.eu>
2023-11-08 12:45:48 +0100

    Création de Basis

---
+++ code/headers/basis.h
@@ -0,0 +1,103 @@
+/**
+ * @file basis.h
+ * @author Louati Roméo
+ */
+
+#ifndef BASIS_H
+#define BASIS_H
+#include <armadillo>
+#include <iostream>
+
+/**
+ * @brief Basis contains:
+ * - 4 values br, bz, N, Q, for the basis deformation and the basis truncation
+ * - mMax, nMax, n_zMax, possibles values for m,n,n_z
+ */
+class Basis
+{
+public:
+    /**
+     * @brief br is a basis deformation parameter for rPart
+     *
+     */
+    double br;
+
+    /**
+     * @brief bz is a basis deformation parameter for zPart
+     *
+     */
+    double bz;
+
+    /**
+     * @brief N is a basis truncation parameter
+     *
+     */
+    uint N;
+
+    /**
+     * @brief Q is a basis truncation parameter
+     *
+     */
+    float Q;
+
+    /**
+     * @brief mMax is the maximum value for the quantum number m (excluded)
+     *
+     */
+    int mMax;
+
+    /**
+     * @brief nMax are the maximum values for the quantum number n (excluded)
+     *
+     */
+    arma::ivec nMax;
+
+    /**
+     * @brief n_vMax are the maximul values for the quantum number n_z (excluded)
+     *
+     */
+    arma::imat n_zMax;
+
+    /**
+     * @brief Construct a new Basis object, with the basis deformation parameters and the basis truncation parameters
+     *
+     */
+    Basis(double, double, uint, float);
+
+    /**
+     * @brief gives the corresponding value to m
+     *
+     * @return float n
+     */
+    float nMax(int);
+
+    /**
+     * @brief gives the corresponding value to m and n
+     *
+     * @return float n_z
+     */
+    float n_zMax(int, int);
+
+    /**
+     * @brief compute the rPart of the basis function
+     *
+     * @return arma::vec
+     */
+    arma::vec rPart(arma::vec, int, int);
+
+    /**
+     * @brief compute the zPart of the basis function
+     *
+     * @return arma::vec
+     */
+    arma::vec zPart(arma::vec, int);
+
+    /**
+     * @brief compute the basis function
+     *
+     * @return arma::mat
+     */
+    arma::mat basisFunc(int, int, int, arma::vec, arma::vec);
+};
+
+#endif
---
+++ code/src/basis.cxx
@@ -0,0 +1,107 @@
+/**
+ * @file basis.cxx
+ * @author Louati Roméo
+ * @brief implementation of basis.h
+ *
+ */
+
+#include "../headers/basis.h"
+#include <cmath>
+using namespace arma;
+
+/**
+ * @brief used for the basis truncation
+ *
+ * @param i
+ * @return float
+ */
+float v(int i, int N, float Q)
+{
+    return (N + 2) * pow(Q, 2 / 3) + 0.5 - i * Q;
+}
+/**
+ * @brief Construct a new Basis:: Basis object
+ *
+ * @param br
+ * @param bz
+ * @param N
+ * @param Q
+ */
+Basis::Basis(double brInput, double bzInput, uint NInput, float QInput)
+{
+    br = brInput;
+    bz = bzInput;
+    N = NInput;
+    Q = QInput;
+
+    int i = 0;
+    float res = v(i, N, Q);
+    while (res < 1)
+    {
+        i++;
+        res = v(i, N, Q);
+    }
+    mMax = i;
+
+    arma::ivec nMax(mMax);
+    nMax.fill(0);
+    // TODO
+};
+
+/**
+ * @brief
+ *
+ * @param m
+ * @return float
+ */
+float Basis::nMax(int m)
+{
+    // a faire dans le constructeur puis on recup via le vecteur
+    return 0.5 * (mMax - m - 1) + 1;
+};
+
+/**
+ * @brief
+ *
+ * @param m
+ * @param n
+ * @return float
+ */
+float Basis::n_zMax(int m, int n)
+{
+    // a faire dans le constructeur puis on recup via la matrice
+    return v(m + 2 * n + 1, N, Q);
+};
+
+/**
+ * @brief compute the rPart of the basis function
+ *
+ * @return arma::vec
+ */
+arma::vec Basis::rPart(arma::vec, int, int)
+{
+    // TODO
+    return 0;
+};
+
+/**
+ * @brief compute the zPart of the basis function
+ *
+ * @return arma::vec
+ */
+arma::vec Basis::zPart(arma::vec, int)
+{
+    // TODO
+    return 0;
+};
+
+/**
+ * @brief compute the basis function
+ *
+ * @return arma::mat
+ */
+arma::mat Basis::basisFunc(int, int, int, arma::vec, arma::vec)
+{
+    // TODO
+    return 0;
+};
