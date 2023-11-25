bd290d1f8817eee758964c11b609b83658baa023
Matthias Lapu <matthias.lapu@ensiie.eu>
2023-11-25 14:03:36 +0100

    Ajout timer + fichier résultat

--- code/src/main.cxx
+++ code/src/main.cxx
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <armadillo>
 #include <cmath>
+#include <chrono>
 #include "../headers/basis.h"
 #include "../headers/poly.h"
 using namespace arma;
 
-std::string cubeToDf3(const arma::cube& m)
+std::string cubeToDf3(const arma::cube &m)
 {
   std::stringstream ss(std::stringstream::out | std::stringstream::binary);
   int nx = m.n_rows;
@@ -39,6 +40,8 @@ int main()
 
   rho.load("./code/src/rho.arma", arma_ascii);
 
+  auto start = std::chrono::high_resolution_clock::now();
+
   Basis basis(1.935801664793151, 2.829683956491218, 14, 1.3);
 
   uint i = 0;
@@ -65,7 +68,7 @@ int main()
           {
             for (int n_zp = 0; n_zp < basis.n_zMax(mp, np); n_zp++)
             {
-              //cout << "m = " << m << " n = " << n << " n_z = " << n_z << " mp = " << mp << " np = " << np << " n_zp = " << n_zp << endl;
+              // cout << "m = " << m << " n = " << n << " n_z = " << n_z << " mp = " << mp << " np = " << np << " n_zp = " << n_zp << endl;
               arma::mat funcA = basis.basisFunc(m, n, n_z, zVals, rVals);
               arma::mat funcB = basis.basisFunc(mp, np, n_zp, zVals, rVals);
               result += funcA % funcB * rho(i, j);
@@ -77,6 +80,9 @@ int main()
       }
     }
   }
+
+  // temps en seconde
+  std::cout << "Temps d'exécution : " << std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - start).count() << "s" << std::endl;
 
   result.save("./bin/test.csv", csv_ascii);
   // result.print();
