5a18de9301e683773f70d7066286bd4b839865fb
Matthias Lapu <matthias.lapu@ensiie.eu>
2023-11-25 14:27:47 +0100

    modification commentaire main

--- code/src/main.cxx
+++ code/src/main.cxx
@@ -45,13 +45,6 @@ int main()
   Basis basis(1.935801664793151, 2.829683956491218, 14, 1.3);
 
   uint i = 0;
-  // for (int m = 0; m < basis.mMax; m++)
-  //   for (int n = 0; n < basis.nMax(m); n++)
-  //     for (int n_z = 0; n_z < basis.n_zMax(m, n); n_z++)
-  //     {
-  //       std::cout << "Basis vector " << i << ": m=" << m << " n=" << n << " n_z=" << n_z << std::endl;
-  //       i++;
-  //     }
   mat result = zeros(64, 64); // number of points on r- and z- axes
   vec zVals = linspace(-20, 20, 64);
   vec rVals = linspace(-20, 20, 64);
