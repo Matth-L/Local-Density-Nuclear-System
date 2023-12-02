4906798f9cb4d8d06ca6db68386499d8e547d11e
Matthias Lapu <matthias.lapu@ensiie.eu>
2023-12-02 12:27:10 +0100

    modification compteur

--- code/src/plot.py
+++ code/src/plot.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 mat = np.loadtxt('bin/test.csv',delimiter=',',dtype=float)
-mat2 = np.loadtxt('bin/naive.csv',delimiter=',',dtype=float)
 
 plt.figure(figsize=(10, 5))
 
@@ -13,12 +12,4 @@ plt.ylabel("Values of z [fm]")
 plt.title("Plot of the nuclear density for mat")
 plt.colorbar()
 
-plt.subplot(1, 2, 2)
-plt.pcolormesh(mat2)
-plt.xlabel("Values of r [fm]")
-plt.ylabel("Values of z [fm]")
-plt.title("Plot of the nuclear density for naive")
-plt.colorbar()
-
-plt.tight_layout()
 plt.show()
