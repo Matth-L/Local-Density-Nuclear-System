f590a277a683f482703d68654e8370d4d26b6950
Matthias Lapu <matthias.lapu@ensiie.eu>
2023-12-02 12:03:12 +0100

    Déplacement compteur => update benchmark

--- code/src/main.cxx
+++ code/src/main.cxx
@@ -6,7 +6,7 @@
 #include "../headers/poly.h"
 using namespace arma;
 
-std::string cubeToDf3(const arma::cube& m)
+std::string cubeToDf3(const arma::cube &m)
 {
   std::stringstream ss(std::stringstream::out | std::stringstream::binary);
   int nx = m.n_rows;
@@ -40,14 +40,13 @@ int main()
 
   rho.load("./code/src/rho.arma", arma_ascii);
 
-  auto start = std::chrono::high_resolution_clock::now();
-
   Basis basis(1.935801664793151, 2.829683956491218, 14, 1.3);
 
   uint i = 0;
   mat result = zeros(64, 64); // number of points on r- and z- axes
   vec zVals = linspace(-20, 20, 64);
   vec rVals = linspace(-20, 20, 64);
+  auto start = std::chrono::high_resolution_clock::now();
   for (int m = 0; m < basis.mMax; m++)
   {
     for (int n = 0; n < basis.nMax(m); n++)
